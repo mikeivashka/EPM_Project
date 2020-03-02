bae5abe70c160ccc1933d26cead456087e93d970
DimaBelyakovich <dimabelyakovich.mmf@gmail.com>
2020-03-02 18:10:37 +0300

    add UserBaseData

---
+++ src/by/epam/collections/Gender.java
@@ -0,0 +1,6 @@
+package by.epam.collections;
+
+public enum Gender {
+    MALE,
+    FEMALE;
+}
---
+++ src/by/epam/entity/UserBaseData.java
@@ -0,0 +1,69 @@
+package by.epam.entity;
+
+import by.epam.collections.Gender;
+
+public class UserBaseData {
+    private String name;
+    private String surname;
+    private int age;
+    private Gender gender;
+    private int height;
+    private double weight;
+
+    public UserBaseData(String name, String surname, int age, Gender gender, int height, double weight) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+        this.gender = gender;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
