e5fdeb8775821f810a157b4de6bb9f653d8da603
Mikhail Ivashka <mike.ivashka@gmaiul.com>
2020-03-02 17:57:57 +0300

    Product class workflow

--- src/by/epam/entity/Product.java
+++ src/by/epam/entity/Product.java
@@ -1,6 +1,22 @@
 package by.epam.entity;
 
 public class Product {
-    private String name;
+    private String title;
+    private int caloriesCapacity;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCaloriesCapacity() {
+        return caloriesCapacity;
+    }
+
+    public void setCaloriesCapacity(int caloriesCapacity) {
+        this.caloriesCapacity = caloriesCapacity;
+    }
 }
