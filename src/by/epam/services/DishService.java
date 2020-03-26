f266f46fefe37f262df39f2c2e6baa7b6d4b7ec5
Mikhail Ivashka <mike.ivashka@gmaiul.com>
2020-03-26 20:04:32 +0300

    Refactor for DishService

--- src/by/epam/services/DishService.java
+++ src/by/epam/services/DishService.java
@@ -7,24 +7,18 @@ import by.epam.entity.Product;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class DishService {
-    private static DishDao dao;
+public class DishService extends Service<Dish, String, DishDao>{
+    private Dish create(String title, Integer caloriesCapacity, String link, HashMap<Product, Integer> products){
+        Dish ob = new Dish();
+        ob.setTitle(title);
+        ob.setCaloriesCapacity(caloriesCapacity);
+        ob.setIngredients(products);
+        ob.setRecepyLink(link);
+        return  ob;
+    }
 
-    public static boolean add(String title, String caloriesCapacity, String link, HashMap<Product, Integer> products){
-        Dish dish = new Dish();
-        dish.setTitle(title);
-        Integer capacity;
-        try{
-            capacity = Integer.parseInt(caloriesCapacity);
-        }
-        catch (NumberFormatException e){
-            return false;
-        }
-        dish.setCaloriesCapacity(capacity);
-        dish.setIngredients(products);
-        dish.setRecepyLink(link);
-        dao.create(dish);
-        return true;
+    public boolean add(String title, Integer caloriesCapacity, String link, HashMap<Product, Integer> products){
+        return data.add(this.create(title, caloriesCapacity, link, products));
     }
 
     public static boolean update(String title, String caloriesCapacity, String link, HashMap<Product, Integer> products){
